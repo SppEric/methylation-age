3e194cfa995ff1fc679c1d7578f579f88c8e58cf
Eric J Han <ericjasonhan@gmail.com>
2024-12-21 14:30:26 -0500

    Updated with entire pipeline working

--- GenSmoothScatter.py
+++ GenSmoothScatter.py
@@ -5,15 +5,16 @@ from scipy.stats import gaussian_kde
 import pickle
 
 # Define function to perform KDE on data given age and beta vectors
-def smooth_scatter(age_v, beta_v, filename, marker_size=0):
+def smooth_scatter(age_v, beta_v, filename, marker_size=1, bandwidth=0.1, outlier_bandwidth=2, threshold=4):
+    # NOTE: Generated images are 400 x 300
     # Normalize age vector
     ageT_v = (age_v - np.min(age_v)) / (np.max(age_v) - np.min(age_v))
 
     # Make KDE
     data = np.vstack([ageT_v, beta_v])
-    kernel = gaussian_kde(data)
-    x1 = np.linspace(-0.1, 1.1, 500)
-    x2 = np.linspace(-0.1, 1.1, 500)
+    kernel = gaussian_kde(data, bw_method=bandwidth)
+    x1 = np.linspace(-0.1, 1.1, 1000)
+    x2 = np.linspace(-0.1, 1.1, 1000)
     X1, X2 = np.meshgrid(x1, x2)
     Z = kernel(np.vstack([X1.ravel(), X2.ravel()])).reshape(X1.shape)
 
@@ -21,19 +22,21 @@ def smooth_scatter(age_v, beta_v, filename, marker_size=0):
     z_m = (Z - np.min(Z)) / (np.max(Z) - np.min(Z))
 
     # Define color breaks and color map
-    breaks_v = np.concatenate(([-0.1, 0.00001, 0.01], np.arange(0.1, 1.1, 0.1))) # Add parameters?
+    breaks_v = np.concatenate(([-1, 0.0001, 1], np.arange(0.1, 10.1, 0.1))) # Add parameters?
     color_map = plt.cm.colors.ListedColormap(['white'] + [plt.cm.Greens(i) for i in np.linspace(0.2, 1, len(breaks_v) - 2)])
 
     # Create the plot
     fig = plt.figure(figsize=(4, 3))
-    plt.imshow(z_m, extent=(-0.1, 1.1, -0.1, 1.1), cmap=color_map, origin='lower', aspect='auto', vmin=0, vmax=1)
+    plt.imshow(Z, extent=(-0.1, 1.1, -0.1, 1.1), cmap=plt.cm.Greens, origin='lower', aspect='auto', vmin=0, vmax=1)
     plt.plot(ageT_v, beta_v, 'k.', markersize=marker_size)
     plt.axis('off')
+    
+    # # Debugging
+    # plt.show()
 
     # Save results
-    output = open(f"{filename}.pkl", 'wb')
+    output = open(f"{filename}.pkl", 'wb') # NOTE: We are pickling the pyplot graphs, not the generated jpg images!
     pickle.dump(fig, output)
-    #plt.savefig(f"{filename}.jpg")
 
     # Perform cleanup
     plt.close()
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # methylation-age
+
+## Contents Overview
+`data_processing`: Contains script to download the methylation data locally
+
+`images`: Contains example generated images that will be inputs to the model. The model is trained on images without the dots, images with points are to demonstrate underlying distribution
+
+`R_files`: Contains files given originally by the collaborator
+
+`GenSmoothScatter.py`: Transcription of collaborator's graph generation code into Python (original can be found in `R_files/GenSmoothScatter.R`). The file performs a kernel density estimate using `scipy.stats.gaussian_kde` and generates and pickles the graph
+
+`methylation-age-prototype`: This notebook is where everything gets put together
+
